f99db269b4eabc7be1437b2944089dc41d5c75f3
khw <p40103@gmail.com>
2023-06-26 10:07:27 +0800

    feat: get_raw_data update

--- get_raw_data.py
+++ get_raw_data.py
@@ -28,9 +28,7 @@ def get_stock_data():
     # Get ALPHAVANTAGE API KEY from .env file. 
     load_dotenv()
     ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
-    if not ALPHAVANTAGE_API_KEY:
-        print("Error: Alphavantage API Key Not Found.")
-        return
+    assert ALPHAVANTAGE_API_KEY, "Alphavantage API Key Not Found."
 
     date_two_weeks_ago = get_date_two_weeks_ago()
     stocks = ["IBM", "AAPL"]
